0e8a671208b4fcaf28b141cc4a422552ee630204
huanzi-qch <781792871@qq.com>
2020-12-04 11:18:28 +0800

    1、favicon图片；

--- portal/vue.config.js
+++ portal/vue.config.js
@@ -29,5 +29,14 @@ module.exports = {
                 }
             }
         }
+    },
+    pwa:{
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
     }
 };
--- portal/webpack.base.conf.js
+++
@@ -1,6 +0,0 @@
-new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: 'index.html',
-    favicon:'./favicon.ico',
-    inject: true
-});
